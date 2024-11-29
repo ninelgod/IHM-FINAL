2669928308ffff2cbb333db36346e3bde08a8e03
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2024-11-28 20:18:40 -0500

    .

--- js/configpriv.js
+++ js/configpriv.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
   $("#header").load("header.html");
   $("#footer").load("footer.html");
   $("#seccion-catalogo").load("catalogo.html");
-  $("#nuestrosValores").load("nuestrosValores.html");
+  $("#valores").load("valores.html");
+  $("#politica").load("politica.html");
+  $("#reclamaciones").load("reclamaciones.html");
 });
