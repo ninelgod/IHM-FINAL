16678e81a1944a426640523cb0516deba4a53daa
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2024-11-28 19:36:15 -0500

    js nuestros valores

--- js/configpriv.js
+++ js/configpriv.js
@@ -3,4 +3,5 @@ $(document).ready(function() {
   $("#header").load("header.html");
   $("#footer").load("footer.html");
   $("#seccion-catalogo").load("catalogo.html");
+  $("#nuestrosValores").load("nuestrosValores.html");
 });
